a1135f5cd7f32f9ac381ea1ed84021ebf40c0f84
Josh Turgeon <j_turgeon88@yahoo.com>
2018-09-19 14:45:24 -0500

    route changes triggered

--- bundle.js
+++ bundle.js
@@ -67,7 +67,21 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log("It's working")
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  window.location.hash = "#inbox";
+  let navItems = Array.from(document.querySelectorAll(".sidebar-nav li"));
+  navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+      let name = navItem.innerText.toLowerCase();
+      location.hash = name;
+    });
+  });
+});
 
 
 /***/ })
